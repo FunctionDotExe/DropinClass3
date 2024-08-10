12b7e99fe5e31da75828344f7952a89b139de2be
FunctionDotExe <89425469+FunctionDotExe@users.noreply.github.com>
2024-08-10 21:17:06 +0000

    Initial commit

---
+++ step2.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where =Australia'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='ResultsContainer')
+
+print(print(results.prettify()))
